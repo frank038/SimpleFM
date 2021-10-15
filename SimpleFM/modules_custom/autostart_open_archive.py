fa740fb64f16711ac8b54092df215bacf1fa97d4
frank038 <42263206+frank038@users.noreply.github.com>
2021-10-15 15:32:02 +0200

    Add files via upload

--- SimpleFM/modules_custom/autostart_open_archive.py
+++ SimpleFM/modules_custom/autostart_open_archive.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python3
 
 """
-open a supported archive with archivemount
+archivemount program service
+find all archive mounted by archivemount
+autostart module name must be in the form: autostart_WHATEVER_NAME
 """
 import os
-import stat
 from PyQt5.QtWidgets import (QDialog,QBoxLayout,QLabel,QPushButton,QApplication,QMenu)
-from PyQt5.QtGui import (QIcon,QFont)
-from PyQt5.QtCore import (Qt,QFileInfo,QMimeDatabase,QSize)
-import shutil
-import subprocess
+from PyQt5.QtGui import (QIcon)
+from PyQt5.QtCore import (Qt,QSize)
 from cfg import BUTTON_SIZE
 
-#  module_type: this appears in the menu
+#  module_name: useless
 def mmodule_name():
-    return "Open archive"
+    return "Autostart archivemount"
 
-# 1 : one item selected - 2 : more than one item selected - 3 : one or more items selected- 4 on background - 5 always
+# 1 : one item selected - 2 : more than one item selected - 3 : one or more items selected- 4 on background - 5 always - 6 autostart
 # action type
-def mmodule_type(mainLView):
-    #
-    if shutil.which("archivemount"):
-        if mainLView.selection:
-            index = mainLView.selection[0]
-            path = mainLView.fileModel.fileInfo(index).absoluteFilePath()
-            fileInfo = QFileInfo(path)
-            imime = QMimeDatabase().mimeTypeForFile(path, QMimeDatabase.MatchDefault)
-            if imime.name() in ["application/x-cd-image","application/zip","application/x-compressed-tar","application/x-tar","application/x-bzip-compressed-tar","application/x-xz-compressed-tar"]:
-                return 1
-    return 0
-    
+def mmodule_type(win):
+    return 6
+
 #
 class ModuleCustom():
-    def __init__(self, mainLView):
-        index = mainLView.selection[0]
-        path = mainLView.fileModel.fileInfo(index).absoluteFilePath()
+    def __init__(self, win):
+        self.win = win
         base_dest_dir = "/tmp/archivemount"
         try:
             if not os.path.exists(base_dest_dir):
@@ -43,44 +32,44 @@ class ModuleCustom():
             MyDialog("ERROR", "\n{}".format(str(E)))
         #
         self.fuse_mounted = os.path.join(base_dest_dir, "fuse_mounted")
-        file_name = os.path.basename(path)
-        mount_name = file_name
-        #
-        if os.access(path, os.R_OK):
-            try:
-                i = 1
-                while os.path.exists(os.path.join(base_dest_dir, mount_name)):
-                    mount_name = file_name+"_{}".format(i)
-                    i += 1
+        try:
+            # empty or create an useless file
+            if not os.listdir(base_dest_dir):
+                try:
+                    with open(self.fuse_mounted, "w") as ff:
+                        ff.write("")
+                except Exception as E:
+                    MyDialog("ERROR", "\n{}.".format(str(E)))
+            ### get the mounted archives
+            # file name - mount point name (without /tmp/archivemount)
+            fuse_mounted_list = []
+            with open(self.fuse_mounted, "r") as ff:
+                fuse_mounted_list = ff.readlines()
+            # add the buttons
+            for idx in range(0,len(fuse_mounted_list),2):
+                file_name = fuse_mounted_list[idx].strip("\n")
+                mount_name = fuse_mounted_list[idx+1].strip("\n")
                 dest_dir = os.path.join(base_dest_dir, mount_name)
-                os.mkdir(dest_dir)
                 #
-                ret = os.system("archivemount {} {}". format(path, dest_dir))
-                if ret == 0:
-                    # add a button
-                    self.win = mainLView.window
-                    self.media_btn = QPushButton(QIcon("icons/fuse-archive.png"),"")
+                if os.path.exists(dest_dir):
+                    self.media_btn = QPushButton(QIcon("icons/fuse-archive.png"),None)
                     if BUTTON_SIZE:
                         self.media_btn.setIconSize(QSize(BUTTON_SIZE, BUTTON_SIZE))
-                    self.win.disk_box.addWidget(self.media_btn)
+                    # 
                     if mount_name == file_name:
                         self.media_btn.setToolTip(file_name)
                     else:
                         self.media_btn.setToolTip(file_name+" - ("+mount_name+")")
+                    #
                     self.media_btn_menu = QMenu()
                     self.media_btn_menu.addAction("Open", lambda x=dest_dir: self.win.openDir(x, 1))
                     self.media_btn_menu.addAction("Umount", lambda x=dest_dir,y=self.media_btn: self.fuserumountf(x,y))
                     self.media_btn.setMenu(self.media_btn_menu)
-                    # 
-                    with open(self.fuse_mounted, "a") as ff:
-                        ff.write("{}\n{}\n".format(file_name, mount_name))
-                    #
-                    MyDialog("Info", "{}\nmounted".format(file_name))
-                else:
-                    MyDialog("Info", "{}\nError: {}".format(file_name, ret))
-            except Exception as E:
-                MyDialog("ERROR", "Issues while opening the archive:\n{}\n{}".format(file_name, str(E)))
+                    self.win.disk_box.addWidget(self.media_btn)
+        except Exception as E:
+            MyDialog("ERROR", "\n{}.".format(str(E)))
     
+
     # umount the mounted archive
     def fuserumountf(self, dest_dir, btn):
         ret = os.system("fusermount -u {}".format(dest_dir))
@@ -91,7 +80,6 @@ class ModuleCustom():
                 fuse_mounted_list = []
                 with open(self.fuse_mounted, "r") as ff:
                     fuse_mounted_list = ff.readlines()
-                #
                 len_fuse_mounted_list = len(fuse_mounted_list)
                 #
                 for idx in range(len_fuse_mounted_list-1, -1, -2):
@@ -108,7 +96,7 @@ class ModuleCustom():
         else:
             MyDialog("Info", "Error while umount the folder\n{}\n{}".format(dest_dir, ret))
 
-    
+
 class MyDialog(QDialog):
     def __init__(self, *args, parent=None):
         super(MyDialog, self).__init__(parent)
