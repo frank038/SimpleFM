d4b30ce389564effde7c27b1a1e0305f842bae73
frank038 <42263206+frank038@users.noreply.github.com>
2022-08-26 14:54:54 +0200

    Add files via upload

--- SimpleFM/cfg.py
+++ SimpleFM/cfg.py
@@ -63,6 +63,8 @@ USE_DELETE = 1
 USE_TRASH = 1
 # load the media: 0 no - 1 yes
 USE_MEDIA = 1
+# media to skip, comma separated values in the form "/dev/xxx" - not reccomanded for removable ones
+MEDIA_SKIP=[]
 # Paste and Merge, how to backup the new files: 0 add progressive number
 # in the form _(#) - 1 add date and time (without checking eventually
 # existing file at destination with same date and time suffix) 
